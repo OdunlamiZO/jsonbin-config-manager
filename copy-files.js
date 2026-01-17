19021f41098ad21069d411e1e878336ec2ba61f4
Odunlami Zacchaeus <odunlamizacchaeus@gmail.com>
2026-01-17 10:51:21 +0100

    update pre-commit (#15)
    * update pre-commit
    
    * update
    
    * update
    
    * update
    
    * update

--- copy-files.js
+++ copy-files.js
@@ -4,16 +4,21 @@ var fs = require('fs');
 
 ncp.limit = 16;
 
-ncp('./src/main/resources', 'target/classes', {
+ncp(
+  './src/main/resources',
+  'target/classes',
+  {
     filter: (source) => {
-        if (fs.lstatSync(source).isDirectory()) {
-            return true;
-        } else {
-            return source.match(process.argv[2]) != null;
-        }
-    }
-}, function (err) {
+      if (fs.lstatSync(source).isDirectory()) {
+        return true;
+      } else {
+        return source.match(process.argv[2]) != null;
+      }
+    },
+  },
+  function (err) {
     if (err) {
-        return console.error(err);
+      return console.error(err);
     }
-});
+  },
+);
